710f9de751ece69317cbcf13702a75f17af70ff3
Babar Bilal <babarxm2@gmail.com>
2016-11-21 02:14:30 +0500

    some fixes

--- src/custom-typings.d.ts
+++ src/custom-typings.d.ts
@@ -1,6 +1,5 @@
 // Extra variables that live on Global that will be replaced by webpack DefinePlugin
 declare var ENV: string;
-declare var FEATURE_REGISTRY: Object;
 declare var WEBPACK_THEME: Object;
 declare module "textarea-caret" {
   function getCaretCoordinates(element: any, selection: number, options?: { debug: boolean; }): { top: number; left: number; };
